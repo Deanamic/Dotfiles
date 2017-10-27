abd260dc2b2d23071c818152522d65cfd79d956f
Dean Zhu <deanzhu1@gmail.com>
2017-10-27 14:35:51 +0200

    updated emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,26 @@
-
+(setq package-list '(
+		     neotree
+		     matlab-mode
+		     chess
+		     auctex
+		     darkokai-theme
+		     company
+             clean-aindent-mode
+             dtrt-indent
+             ws-butler
+		     ))
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 ;disable suspend
 (global-unset-key [(control z)])
 
@@ -17,6 +33,7 @@
 ;; No splash screen
 (setq inhibit-startup-message t)
 
+(show-paren-mode 1)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -92,17 +109,58 @@
 
 (global-set-key (kbd "<C-S-down>") 'move-line-down)
 (global-set-key (kbd "<C-S-up>") 'move-line-up)
+(global-set-key (kbd "<f8>") 'neotree-toggle)
 
+(load-theme 'darkokai t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (neotree))))
+ '(custom-safe-themes
+   (quote
+    ("6ee6f99dc6219b65f67e04149c79ea316ca4bcd769a9e904030d38908fd7ccf9" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" default)))
+ '(package-selected-packages
+   (quote
+    (ws-butler drtr-indent company company-mode darkokai darkokai-theme monokai-alt-theme monokai-theme matlab-mode auctex neotree)))
+ '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;;complete anything
+(add-hook 'after-init-hook 'global-company-mode)
+
+;;floding
+(add-hook 'c-mode-common-hook   'hs-minor-mode)
+
+;;endline + indent
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+;; activate whitespace-mode to view all whitespace characters
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+;; show unncessary whitespace that can mess up your diff
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+
+;; use space to indent by default
+(setq-default indent-tabs-mode nil)
+
+;; set appearance of a tab that is represented by 4 spaces
+(setq-default tab-width 4)
+
+;; Package: clean-aindent-mode
+(require 'clean-aindent-mode)
+(add-hook 'prog-mode-hook 'clean-aindent-mode)
+
+;; Package: dtrt-indent
+(require 'dtrt-indent)
+(dtrt-indent-mode 1)
+
+(require 'ws-butler)
+(add-hook 'c-mode-common-hook 'ws-butler-mode)
