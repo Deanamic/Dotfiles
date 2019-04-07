0bd03fbd49d55ceb2e019122204cbd274d6c2c5e
Dean Zhu <deanzhu1@gmail.com>
2019-04-07 12:40:34 +0200

    desmume and packages.org

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
-#!/bin/zsh
+#!/bin/sh
+_SPACEMACSREPO = https://github.com/syl20bnr/spacemacs
+_DOTFILESREPO = https://github.com/Deanamic/Dotfiles.git
+exit 0
+
+echo "Updating Repositories"
 pacman -Syu
-pacman -S emacs vim zsh zsh-syntax-highlighting yay python-pip zathura nemo nemo-share samba manjaro-settings-samba
+echo "Downloading Text Editors"
+pacman -S emacs vim
+echo "Downloading and Configuring Spacemacs"
+mkdir ~/Github
+git clone ${_SPACEMACSREPO} ~/.emacs.d
+git clone ${_DOTFILESREPO} ~/Github/
+cp ~/Github/.spacemacs ~/
+
+pacman -S zsh zsh-syntax-highlighting yay python-pip zathura nemo nemo-share samba manjaro-settings-samba
